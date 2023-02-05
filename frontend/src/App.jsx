7cfa09a3e7de875a49e4a886939894fd9dd06ca9
Thomas Dejardins <tdejardins@gmail.com>
2023-02-05 22:23:10 +0100

    feat(App.jsx): add Contact page and Products page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
 }
