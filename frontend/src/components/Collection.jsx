a451df8a40cfe1ff1d373f1e13f90da36cb04f9b
Thomas Dejardins <tdejardins@gmail.com>
2023-02-08 11:14:41 +0100

    feat(Collection.jsx): add link from react-router-dom

--- frontend/src/components/Collection.jsx
+++ frontend/src/components/Collection.jsx
@@ -1,4 +1,5 @@
 import "../styles/components/Collection.css";
+import { Link } from "react-router-dom";
 
 function Collection(props) {
   const data = props.products;
@@ -6,13 +7,12 @@ function Collection(props) {
   return (
     <section className="products-container">
       {data.map((product, index) => (
-        <div key={index} className="product">
+        <Link to={"/product/" + product.id} key={index} className="product">
           <h2>{product.title}</h2>
           <h3>{product.brand}</h3>
           <img src={product.image} alt={product.title} />
-
           <p>{product.price} €</p>
-        </div>
+        </Link>
       ))}
     </section>
   );
