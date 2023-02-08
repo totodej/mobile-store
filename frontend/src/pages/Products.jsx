102e7c7a059f4f0b23b8dc554b634ae4e13c9930
Thomas Dejardins <tdejardins@gmail.com>
2023-02-08 11:19:41 +0100

    feat(Products.jsx): add filterBrand function and useLocation

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,9 +1,14 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Collection from "../components/Collection";
 
 function Products() {
   const [data, setData] = useState(null);
+  const location = useLocation();
+  const queryParameters = new URLSearchParams(location.search);
+  const brand = queryParameters.get("brand");
 
   useEffect(() => {
     fetch("http://localhost:5000/api/products")
@@ -15,9 +20,20 @@ function Products() {
     return null;
   }
 
+  const filterBrand = () => {
+    const filterProducts = [];
+    data.forEach((product) => {
+      if (product.brand === brand) {
+        filterProducts.push(product);
+      }
+    });
+    return filterProducts;
+  };
+
   return (
     <div>
       <Header products={data} />
+      <Collection products={filterBrand()} />
       <Footer />
     </div>
   );
