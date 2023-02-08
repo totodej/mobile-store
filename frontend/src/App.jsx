986f66659febb57a4317cf2e5ffdb55173d95133
Thomas Dejardins <tdejardins@gmail.com>
2023-02-08 11:08:44 +0100

    feat(App.jsx): add Product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Products from "./pages/Products";
+import Product from "./pages/Product";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/products" element={<Products />} />
+      <Route path="/product/:id" element={<Product />} />
     </Routes>
   );
 }
