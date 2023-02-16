be62def319aead1c5a9fb2cae11002228cf3a960
Thomas Dejardins <tdejardins@gmail.com>
2023-02-16 15:57:29 +0100

    feat(App.jsx): add Cart Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Products from "./pages/Products";
 import Product from "./pages/Product";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/contact" element={<Contact />} />
       <Route path="/products" element={<Products />} />
       <Route path="/product/:id" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
     </Routes>
   );
 }
