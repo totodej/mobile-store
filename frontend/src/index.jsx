94a8a81b62c0c1be3a39b5e1dfdee7326e7578cd
Thomas Dejardins <tdejardins@gmail.com>
2023-02-27 17:54:52 +0100

    feat(index.jsx): add Provider

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </HashRouter>
   </React.StrictMode>
 );
