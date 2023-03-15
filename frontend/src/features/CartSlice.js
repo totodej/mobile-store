eed0a221d88abd3af93fdabbd272421c3b46be71
Thomas Dejardins <tdejardins@gmail.com>
2023-03-15 12:08:28 +0100

    feat(CartSlice.js): add incrementQuantity and decrementQuantity

--- frontend/src/features/CartSlice.js
+++ frontend/src/features/CartSlice.js
@@ -7,14 +7,39 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart(state, action) {
-      state.push(action.payload);
+      const itemInCart = state.find((item) => item.id === action.payload.id);
+      if (itemInCart) {
+        itemInCart.quantity++;
+      } else {
+        state.push({ ...action.payload, quantity: 1 });
+      }
     },
     removeFromCart(state, action) {
       return state.filter((item) => item.id !== action.payload.id);
     },
+    incrementQuantity: (state, action) => {
+      const itemInCart = state.find((item) => item.id === action.payload.id);
+      itemInCart.quantity++;
+    },
+    decrementQuantity: (state, action) => {
+      const itemInCart = state.find((item) => item.id === action.payload.id);
+      if (itemInCart.quantity === 1) {
+        const removeFromCart = state.filter(
+          (item) => item.id !== action.payload.id
+        );
+        state = removeFromCart;
+      } else {
+        itemInCart.quantity--;
+      }
+    },
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  incrementQuantity,
+  decrementQuantity,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
